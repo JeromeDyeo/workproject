45edd11817c307a6d6faf1d9bbde5edd415a53f2
jerome.dyeo <jerome.dyeo.2015@smu.edu.sg>
2018-05-18 14:10:28 +0800

    created base project

---
+++ DataDictionary/src/main/java/com/datadictionary/DataDictionaryApplication.java
@@ -0,0 +1,12 @@
+package com.datadictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataDictionaryApplication.class, args);
+	}
+}
---
+++ DataDictionary/src/test/java/com/datadictionary/DataDictionaryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.datadictionary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataDictionaryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
